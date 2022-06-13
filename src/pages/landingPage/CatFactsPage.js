60a2646e23e2db0f0ad46dd4269fe941368cfa1a
OysteinKoder <oystein.kodehode@gmail.com>
2022-06-13 07:50:29 +0200

    addded cat fact api call

--- src/pages/landingPage/CatFactsPage.js
+++ src/pages/landingPage/CatFactsPage.js
@@ -1,14 +1,34 @@
 import { StyledMain } from "../../components/styles";
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
 
 const CatFactPage = () => {
-  return (
-    <StyledMain>
-      <h2>Cat Fact Page</h2>
-      <article>
-        <p>this is the cat facts page</p>
-      </article>
-    </StyledMain>
-  );
+  const [data, setData] = useState();
+  const getCatData = () => {
+    axios.get("https://catfact.ninja/facts").then((res) => {
+      setData(res.data.data);
+    });
+  };
+  console.log(data);
+  useEffect(() => {
+    getCatData();
+  }, []);
+
+  if (data) {
+    return (
+      <StyledMain>
+        <h2>Cat Fact Page</h2>
+        <article>
+          {data.map((item, idx) => {
+            return <p key={idx}>{item.fact}</p>;
+          })}
+        </article>
+      </StyledMain>
+    );
+  } else {
+    return <div>loading</div>;
+  }
 };
 
 export default CatFactPage;
