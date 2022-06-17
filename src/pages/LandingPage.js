e88ec1f42e5a1e27de6be808de3d6dfd04bbfb36
OysteinKoder <oystein.kodehode@gmail.com>
2022-06-17 09:50:34 +0200

    changed prop example stuff

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -2,7 +2,7 @@ import { Routes, Route, Link } from "react-router-dom";
 import AboutUsPage from "./landingPage/AboutUsPage";
 import CatFactPage from "./landingPage/CatFactsPage";
 import PantePersonSelectorPage from "./landingPage/PantePersonSelectorPage";
-import PropsExample from "./landingPage/StudentProjectsPage";
+import PropsExample from "./landingPage/PropsExample";
 import ReactLogo from "../components/ReactLogo";
 import Header from "./landingPage/landingPageFiles/Header";
 import BackgroundParticles from "../components/BackgroundParticles";
@@ -20,17 +20,7 @@ const LandingPage = () => {
           path="/pante-person-selector"
           element={<PantePersonSelectorPage />}
         />
-        <Route
-          path="/props"
-          element={
-            <PropsExample
-              text="dette er text"
-              headline="dette er overskrift"
-              num1={2}
-              num2={3}
-            />
-          }
-        />
+        <Route path="/props" element={<PropsExample />} />
       </Routes>
     </>
   );
