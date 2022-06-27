31752facedf7f3a17787d0eba2f84cdfc07c0d32
OysteinKoder <oystein.kodehode@gmail.com>
2022-06-27 13:30:52 +0200

    merge fixes

--- src/pages/landingPage/CatFactsPage.js
+++ src/pages/landingPage/CatFactsPage.js
@@ -2,53 +2,32 @@ import { StyledButton, StyledMain } from "../../components/styles";
 import { useEffect, useState } from "react";
 
 const CatFactPage = () => {
+  const [catLives, setCatLives] = useState(9);
+
+  const addLives = () => setCatLives(catLives + 1);
+  const removeLives = () => setCatLives(catLives - 1);
 
   useEffect(() => {
-    example();
-    // eslint-disable-next-line
-  }, []);
-
+    setTimeout(addLives, 1000);
+  });
   return (
     <StyledMain>
       <h2>React Hooks, useState/useEffect</h2>
       <article>
         <p>This Cat has: {catLives} lives left</p>
         <StyledButton onClick={addLives}>Add life</StyledButton>
-        <StyledButton onClick={removeCatLives}>Remove life</StyledButton>
+        <StyledButton onClick={removeLives}>Remove life</StyledButton>
       </article>
     </StyledMain>
   );
-
-  if (data) {
-    return (
-      <StyledMain>
-        <h2>Cat Fact Page</h2>
-        <article>
-          {data.map((item, idx) => {
-            return <p key={idx}>{item.fact}</p>;
-          })}
-        </article>
-      </StyledMain>
-    );
-  } else if (apiError) {
-    return (
-      <StyledMain>
-        <h2>Cat Fact Page</h2>
-        <article>
-          <p>Content failed to load, please try again</p>
-        </article>
-      </StyledMain>
-    );
-  } else {
-    return (
-      <StyledMain>
-        <h2>Cat Fact Page</h2>
-        <article>
-          <p>The content is loading</p>
-        </article>
-      </StyledMain>
-    );
-  }
+  return (
+    <StyledMain>
+      <h2>Cat Fact Page</h2>
+      <article>
+        <p>Cat lives : {catLives}</p>
+      </article>
+    </StyledMain>
+  );
 };
 
 export default CatFactPage;
