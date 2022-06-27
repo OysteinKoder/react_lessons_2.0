755c7e4c071c0a0171c9025599396dc310f4601f
OysteinKoder <oystein.kodehode@gmail.com>
2022-06-27 13:22:56 +0200

    "merge"

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import logo from "../logo.svg";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>React Lessons 2.0</h1>
+      <img src={logo} className="App-logo" />
+    </header>
+  );
+};
+
+export default Header;
