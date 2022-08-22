947637c955c314f47cec6d84753f2f20ea1b696f
OysteinKoder <oystein.kodehode@gmail.com>
2022-08-22 12:56:38 +0200

    further deconstructed textAndContent

--- src/pages/TextExamplePage.js
+++ src/pages/TextExamplePage.js
@@ -2,15 +2,16 @@ import { textAndContent } from "../textAndContent/textAndContent";
 
 const TextExamplePage = () => {
   const { seaVehiclesPage } = textAndContent;
+  const { motorBoat, jetSki } = textAndContent.seaVehiclesPage;
   return (
     <>
       <h1>{seaVehiclesPage.headline}</h1>
-      <h2>{seaVehiclesPage.motorBoat.headline}</h2>
-      <p>{seaVehiclesPage.motorBoat.text}</p>
-      <img src={seaVehiclesPage.motorBoat.image} />
-      <h2>{seaVehiclesPage.jetSki.headline}</h2>
-      <p>{seaVehiclesPage.jetSki.text}</p>
-      <img src={seaVehiclesPage.jetSki.image} />
+      <h2>{motorBoat.headline}</h2>
+      <p>{motorBoat.text}</p>
+      <img src={motorBoat.image} />
+      <h2>{jetSki.headline}</h2>
+      <p>{jetSki.text}</p>
+      <img src={jetSki.image} />
     </>
   );
 };
