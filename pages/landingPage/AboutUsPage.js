e679ff56b9b7bc3645c06f12f32d2779e1058cc5
knut <104001548+knutcode@users.noreply.github.com>
2022-09-08 14:52:25 +0200

    made functional pante person
    Added some animations and uses a data sheet to easily add or remove people from the list.

---
+++ pages/landingPage/AboutUsPage.js
@@ -0,0 +1,14 @@
+import { StyledMain } from "../../components/styles";
+
+const AboutUsPage = () => {
+  return (
+    <StyledMain>
+      <h2>About us</h2>
+      <article>
+        <p>this is the about us page</p>
+      </article>
+    </StyledMain>
+  );
+};
+
+export default AboutUsPage;
---
+++ pages/landingPage/PantePersonSelectorPage.js
@@ -0,0 +1,42 @@
+import { StyledButton, StyledMain } from "../../components/styles";
+import { useState } from "react";
+import { PanteSelectorTitle, RandomPerson } from "./PantePersonSelectorFiles/PantePersonSelectorStyles";
+import people from "./PantePersonSelectorFiles/PantePersonData";
+
+const PantePersonSelectorPage = () => {
+  const [pantePerson, setPantePerson] = useState("Welcome!");
+  const [personList, setPersonList] = useState([...people]);
+  const [newName, setNewName] = useState(false);
+
+  const getRandomPerson = () => {
+    setNewName(true);
+    console.log(personList);
+    const randomNumber = Math.floor(Math.random() * personList.length);
+    const vinner = personList[randomNumber];
+
+    setPantePerson(vinner);
+
+    personList.splice(randomNumber, 1);
+    setPersonList(personList);
+
+    setTimeout(() => {
+      setNewName(false);
+    }, 10500);
+  };
+
+  return (
+    <StyledMain>
+      <PanteSelectorTitle>Pante selector</PanteSelectorTitle>
+      <article>
+        <RandomPerson className={newName && "fade"}>
+          {pantePerson ? pantePerson : "No more people to select from"}
+        </RandomPerson>
+      </article>
+      <StyledButton onClick={getRandomPerson} disabled={newName}>
+        Get random person
+      </StyledButton>
+    </StyledMain>
+  );
+};
+
+export default PantePersonSelectorPage;
