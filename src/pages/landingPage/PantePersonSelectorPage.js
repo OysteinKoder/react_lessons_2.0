a8847eddb77b00de46151ac29c3752a40f945cb7
Stian Kodehode <stiant.kodehode@gmail.com>
2022-09-09 10:48:01 +0200

    confetti fix

--- src/pages/landingPage/PantePersonSelectorPage.js
+++ src/pages/landingPage/PantePersonSelectorPage.js
@@ -1,8 +1,9 @@
 import { StyledButton, StyledMain } from "../../components/styles";
-import { useState } from "react";
+import { useState, useCallback, useEffect } from "react";
 import pantePersons from "./pantePersonSelectorPage/pantePersonsArray";
 import { StyledContainer } from "./pantePersonSelectorPage/components/styled";
 import PantePerson from "./pantePersonSelectorPage/components/PantePerson";
+import Confetti from "react-confetti";
 
 const PantePersonSelectorPage = () => {
     const [allPantePersons, setAllPantePersons] = useState(pantePersons);
@@ -14,18 +15,30 @@ const PantePersonSelectorPage = () => {
         setPantePerson([allPantePersons[randomIndex]]);
     };
 
+    // Width/Height for the confetti
+    const [height, setHeight] = useState();
+    const [width, setWidth] = useState();
+
+    // This updates the Height and Width of the container when a panteperson is picked
+    const div = useCallback(
+        (node) => {
+            if (node !== null) {
+                setHeight(node.getBoundingClientRect().height);
+                setWidth(node.getBoundingClientRect().width);
+            }
+        },
+        [pantePerson]
+    );
+
     return (
-        <StyledMain>
+        <StyledMain ref={div}>
+            {pantePerson && <Confetti width={width} height={height} />}
             <h2>Pante Selector</h2>
             <article>
                 <StyledContainer>
                     <PantePerson key={pantePerson} person={pantePerson} />
                 </StyledContainer>
-                <StyledButton
-                    onClick={() => {
-                        pickPantePerson();
-                    }}
-                >
+                <StyledButton onClick={pickPantePerson}>
                     Button of dethhhhhhhhh
                 </StyledButton>
             </article>
--- src/pages/landingPage/pantePersonSelectorPage/components/PantePerson.js
+++ src/pages/landingPage/pantePersonSelectorPage/components/PantePerson.js
@@ -1,11 +1,9 @@
 import { StyledH1NoAnimation, StyledH1 } from "./styled";
-import Confetti from "react-confetti";
 
 const PantePerson = (props) => {
     if (props.person) {
         return (
             <>
-                <Confetti width={900} height={600} />
                 <StyledH1>{props.person}</StyledH1>
             </>
         );
