4ebfc64b01532d507ac1d303593c8522a2b0740d
Morthenjo <104001709+Morthenjo@users.noreply.github.com>
2022-09-09 10:39:06 +0200

    Added newPantePerson
    Nå kan du skrive inn nye folk on the fly

--- src/pages/landingPage/PantePersonSelectorPage.js
+++ src/pages/landingPage/PantePersonSelectorPage.js
@@ -4,6 +4,7 @@ import { StyledMain } from "../../components/styles";
 const PantePersonSelectorPage = () => {
   const [pantePerson, setPantePerson] = useState("Her kommer panteper");
   const [pantePerson2, setPantePerson2] = useState("Her kommer pantejenny");
+  let [newPantePerson, setNewPantePerson] = useState("");
   let [pantePersonList, setPantePersonList] = useState([
     "Morthen",
     "Knut",
@@ -14,23 +15,27 @@ const PantePersonSelectorPage = () => {
     "Øystein",
   ]);
 
+  const addclick = (e) => {
+    e.preventDefault();
+    if (newPantePerson.trim().length !== 0) {
+      setPantePersonList([...pantePersonList, newPantePerson]);
+      setNewPantePerson("");
+    }
+  };
+
   function handleclick() {
     console.log([...pantePersonList]);
     let vinner =
       pantePersonList[Math.floor(Math.random() * pantePersonList.length)];
     setPantePerson(vinner);
-    console.log(vinner);
     let vinInd = pantePersonList.indexOf(vinner);
     pantePersonList.splice(vinInd, 1);
     let vinner2 =
       pantePersonList[Math.floor(Math.random() * pantePersonList.length)];
     setPantePerson2(vinner2);
-    console.log(vinner2);
     let vinInd2 = pantePersonList.indexOf(vinner2);
     pantePersonList.splice(vinInd2, 1);
     setPantePersonList(pantePersonList);
-    console.log(vinInd);
-    console.log(vinInd2);
   }
 
   return (
@@ -41,6 +46,14 @@ const PantePersonSelectorPage = () => {
         <button onClick={handleclick}>Velg PantePerson</button>
         <p>{pantePerson}</p>
         <p>{pantePerson2}</p>
+
+        <form onSubmit={addclick}>
+          <input
+            onChange={(e) => setNewPantePerson(e.target.value)}
+            value={newPantePerson}
+          ></input>
+          <button type="submit">Add to panteList</button>
+        </form>
 
         <p>Liste over eligible Pantepersoner</p>
         <ul>
