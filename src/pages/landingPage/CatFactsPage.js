0c2c5db3332d6198514eaeae63baa5acaaeaee9f
OysteinKoder <oystein.kodehode@gmail.com>
2022-09-16 09:46:49 +0200

    api call to render cat facts on the catfacts page

--- src/pages/landingPage/CatFactsPage.js
+++ src/pages/landingPage/CatFactsPage.js
@@ -1,14 +1,50 @@
 import { StyledMain } from "../../components/styles";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const CatFactPage = () => {
-  return (
-    <StyledMain>
-      <h2>Cat Fact Page</h2>
+  // const [data, setData] = useState();
+  // const [error, setError] = useState();
+
+  // const apiCall = () => {};
+
+  // useEffect(() => {
+  //   axios("https://catfact.ninja/facts")
+  //     .then((res) => {
+  //       setData(res.data.data);
+  //     })
+  //     .catch((error) => {
+  //       setError(error);
+  //     });
+  // }, []);
+
+  const [data, setData] = useState();
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    axios("https://catfact.ninja/facts")
+      .then((res) => {
+        setData(res.data.data);
+      })
+      .catch((res) => {
+        setError(res);
+      });
+    return setData((data) => null);
+  }, []);
+
+  if (data) {
+    return (
       <article>
-        <p>this is the cat facts page</p>
+        {data.map((item, idx) => {
+          return <p key={idx}>{item.fact}</p>;
+        })}
       </article>
-    </StyledMain>
-  );
+    );
+  } else if (error) {
+    <h2>Failed to load, please try again</h2>;
+  } else {
+    <h2>Content Loading</h2>;
+  }
 };
 
 export default CatFactPage;
