b28e70e89bf816ff99ee806738edd57c282fe2fd
OysteinKoder <oystein.kodehode@gmail.com>
2022-09-20 09:37:22 +0200

    changed cat action payloads to one. (now adds and subs 1 cat-life)

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -3,13 +3,13 @@ import { INCREASE_NUMBER, DECREASE_NUMBER } from "./case";
 export const increase = () => {
   return {
     type: INCREASE_NUMBER,
-    payload: 2,
+    payload: 1,
   };
 };
 
 export const decrease = () => {
   return {
     type: DECREASE_NUMBER,
-    payload: 3,
+    payload: 1,
   };
 };
