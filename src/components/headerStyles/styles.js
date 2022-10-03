f09c0bacb264e782475b87e625af415f38451efd
OysteinKoder <oystein.kodehode@gmail.com>
2022-10-03 10:29:00 +0200

    comment

--- src/components/headerStyles/styles.js
+++ src/components/headerStyles/styles.js
@@ -1,7 +1,19 @@
 import styled from "styled-components";
 
+// we import styled from "styled-components" to tell react that we are using
+// styled components
+// you write normal css inside the two ``
+
 export const StyledHeader = styled.header`
   margin: 0 auto;
   padding: 1rem;
   background-color: #47bbed;
+`;
+
+// you can also build on top of other components by putting the component inside of ()
+// with many css libraries you can also use the components from then in the same way
+// sometimes you have to put !important behind each property though
+// example: color: salmon !important
+export const Paragraph = styled(StyledHeader)`
+  color: salmon;
 `;
