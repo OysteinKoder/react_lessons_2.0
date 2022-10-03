2030db9a119a1ad27809152f2f3ebf7610c63985
OysteinKoder <oystein.kodehode@gmail.com>
2022-10-03 10:20:07 +0200

    comments

--- src/pages/landingPage/UseReduserPage.js
+++ src/pages/landingPage/UseReduserPage.js
@@ -1,9 +1,13 @@
 import { useReducer } from "react";
 
+// contains the initial state used
 const initialState = {
   number: 0,
 };
 
+// the countReducer is a reducer that contains a switch case
+// you can look at switch cases like if else statements
+// example: when the "INCREASE" case is dispatched it changes the state in the return
 const countReducer = (state, action) => {
   switch (action.type) {
     case "INCREASE":
@@ -16,6 +20,8 @@ const countReducer = (state, action) => {
 };
 
 const UseReducerPage = () => {
+  // here we define what reducer we will use (the reducer can change the state)
+  // we also define what state should be changed (initialState)
   const [state, dispatch] = useReducer(countReducer, initialState);
 
   return (
@@ -23,6 +29,8 @@ const UseReducerPage = () => {
       <p>{state.number}</p>
       <button
         onClick={() => {
+          // with the dispatch we tell the reducer what case should be fired in
+          // the reducer
           dispatch({ type: "INCREASE" });
         }}
       >
