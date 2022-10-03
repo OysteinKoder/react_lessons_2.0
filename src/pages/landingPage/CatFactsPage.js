f0d88726c45fea62a85617d72e3b41b3072b8f38
OysteinKoder <oystein.kodehode@gmail.com>
2022-10-03 10:14:53 +0200

    comments

--- src/pages/landingPage/CatFactsPage.js
+++ src/pages/landingPage/CatFactsPage.js
@@ -1,26 +1,12 @@
-import { StyledMain } from "../../components/styles";
-import { useState, useEffect } from "react";
 import axios from "axios";
+import { useState, useEffect } from "react";
 
 const CatFactPage = () => {
-  // const [data, setData] = useState();
-  // const [error, setError] = useState();
-
-  // const apiCall = () => {};
-
-  // useEffect(() => {
-  //   axios("https://catfact.ninja/facts")
-  //     .then((res) => {
-  //       setData(res.data.data);
-  //     })
-  //     .catch((error) => {
-  //       setError(error);
-  //     });
-  // }, []);
-
+  // contains sate for error and the data which will be used in the api call
   const [data, setData] = useState();
   const [error, setError] = useState();
 
+  // contains api call that will be lunched when the component loads
   useEffect(() => {
     axios("https://catfact.ninja/facts")
       .then((res) => {
@@ -35,6 +21,12 @@ const CatFactPage = () => {
   if (data) {
     return (
       <article>
+        {
+          // data.map is an array method that goes through every index in the array
+          // and lets you fire a function for every step
+          // in each step the content in the index is also available
+          // here we return a p containing content from that array index
+        }
         {data.map((item, idx) => {
           return <p key={idx}>{item.fact}</p>;
         })}
