b48728e92dd712a73115dfe22b1d5df8f82d3ba2
OysteinKoder <oystein.kodehode@gmail.com>
2022-10-03 10:08:07 +0200

    comments

--- src/pages/landingPage/CatFactsPage.js
+++ src/pages/landingPage/CatFactsPage.js
@@ -2,24 +2,31 @@ import { StyledButton, StyledMain } from "../../components/styles";
 import { useEffect, useState } from "react";
 
 const CatFactPage = () => {
+  // Contains the state that has catlives in it
+  // is catLives is used when refering to the state
+  // setCatLives is used to change the state (add or remove lives)
   const [catLives, setCatLives] = useState(9);
 
+  // adds lives to the catLives
   const addLives = () => {
     setCatLives((catLives) => catLives + 1);
   };
-
+  // removes lives from catLives
   const removeCatLives = () => {
     setCatLives((catLives) => catLives - 1);
   };
-
-  const example = () => {
+  // the adds a catLife for every 1500 ms when the function is fired
+  const addCatLivesInterval = () => {
     setTimeout(() => {
       setCatLives((catLives) => catLives + 1);
     }, 1500);
   };
 
+  // fires the function when the component is rendered when the [] is empty
+  // if you want to fire the function when a state changes you put [theState]
+  // inside []
   useEffect(() => {
-    example();
+    addCatLivesInterval();
     // eslint-disable-next-line
   }, []);
   return (
